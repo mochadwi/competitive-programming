b743751c5557076bd81bd9a7bc347e2893c5ab31
Mochamad Iqbal Dwi Cahyo <razeluxe20@gmail.com>
2016-12-22 17:44:17 +0700

    [TOKI] Chapter 6 - E. Dua Pangkat

--- 116_looping/problemset/05_dua_pangkat/practice.cpp
+++ 116_looping/problemset/05_dua_pangkat/practice.cpp
@@ -2,26 +2,25 @@
 #include <math.h>
 using namespace std;
 
-int data, bil, pangkat;
+long int data, bil, pangkat;
 string ket;
 
 int main() {
   // Deklarasi + Inisialisasi awal
   data = bil = pangkat = 0;
-  ket = "";
 
   // Data masukkan
   cin >> data;
-  while (bil < data && pangkat < 16) {
+  while (bil < data) {
     bil = pow(2, ++pangkat);    
-    if (bil != data) {
-      ket = "bukan";
-    } else {
-      ket = "ya";
-    }
-    // cout << pangkat << endl;
     // cout << bil << " * ";
   }
-
-  cout << ket << endl;
+  if (bil != data) {
+    // ket = "bukan";
+    cout << "bukan" << endl;
+  } else {
+    // ket = "ya";
+    cout << "ya" << endl;
+  }
+  // cout << ket << endl;
 }
