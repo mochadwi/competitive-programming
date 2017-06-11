ed7de1fb18619c93a686b9552b05cf0201e968e1
Mochamad Iqbal Dwi Cahyo <razeluxe20@gmail.com>
2017-06-11 21:24:11 +0700

    [Update] Using alternatives ways to shorten the time complexity this program runs

--- 118_complexity/problemset/02_check_prime_number/practice.cpp
+++ 118_complexity/problemset/02_check_prime_number/practice.cpp
@@ -1,23 +1,40 @@
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
 int main() {
-  int n, primeNumber;
+  int n, count;
+  int number[1000];
   bool isPrime;
   
   // cout << "How many number: ";
   cin >> n;
 
-  for (int i = 0; i < n; i++) {
-    
-    cin >> primeNumber;
+  for (int i = 0; i < n; i++)
+  {
+    count = 0;  
+    cin >> number[i];
 
-    if (primeNumber > 1 && 
-        (((primeNumber % 2 != 0) && (primeNumber % 3 != 0)) || 
-        (primeNumber == 2) || (primeNumber == 3))) {
-      cout << "YA";
-    } else {
+    if (number[i] > 1)
+    {
+      for (int j = 2; j <= trunc(sqrt(number[i])); j++)
+      {
+        if (number[i] % j == 0)
+        {
+          count++;
+        }
+      }
+      
+      if (count == 0)
+        {
+          cout << "YA";
+        } else
+        {
+          cout << "BUKAN";    
+        }
+    } else
+    {
       cout << "BUKAN";
     }
 
