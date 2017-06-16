5dd8d0d43fe6bb9332f944d6793f63a923e14911
Mochamad Iqbal Dwi Cahyo <razeluxe20@gmail.com>
2017-06-16 16:22:06 +0700

    [Update] #13 Func & Proc - B Tukang Sulap - 100, accepted xD

--- 1113_function_procedure/02_tukang_sulap/practice.cpp
+++ 1113_function_procedure/02_tukang_sulap/practice.cpp
@@ -5,9 +5,14 @@ using namespace std;
 const long limit = 100000;
 int n, q, x, y = 0; // N = col; Q = swap occurs; x, y = col to swap
 long rowA[limit], rowB[limit];
-char a, b; // A, B = row identification
+char a, b; // A, B = row identification; ws = whitespace
 
-// void swap()
+void swap(long &a, long &b)
+{
+  long temp = a;
+  a = b;
+  b = temp;
+}
 
 int main()
 {
@@ -15,25 +20,64 @@ int main()
   for (int i = 0; i < n; i++)
   {
     cin >> rowA[i];
-    cin >> rowB[i];
   }
 
-  // for (int i = 0; i < n; i++)
-  // {
-  //   cout << "A: " << rowA[i] << endl;
-  //   cout << "B: " << rowB[i] << endl;
-  // }
+  for (int i = 0; i < n; i++)
+  {
+    cin >> rowB[i];
+  }
 
   cin >> q;
   for (int i = 0; i < q; i++)
   {
+    char ws;
+    // cin >> a >> ws >> x >> ws >> b >> ws >> y;
     cin >> a >> x >> b >> y;
-    cout << a << x << b << y;
-    if (tolower(a) == 'a' && tolower(b) == 'b')
-    {
-      // swap(a[x-1], b[y-1]);
-    }
+    a = toupper(a);
+    b = toupper(b);
+
+    // cout << a << " " << b;
+
+    // if (x <= n && y <= n)
+    // {
+      if (a == 'A' && b == 'B')
+      {
+        // cout << rowA[x-1] << " " << rowB[y-1] << endl;
+        swap(rowA[x-1], rowB[y-1]);
+        // cout << rowA[x-1] << " " << rowB[y-1] << endl;
+        // cout << a << b;
+      } else if (a == 'A' && b == 'A')
+      {
+        swap(rowA[x-1], rowA[y-1]);
+      } else if (a == 'B' && b == 'A')
+      {
+        swap(rowB[x-1], rowA[y-1]);
+      } else if (a == 'B' && b == 'B')
+      {
+        swap(rowB[x-1], rowB[y-1]);
+      }
+    // }
   }
+
+  for (int i = 0; i < n; i++)
+  {
+    // cout << "A: " << rowA[i] << " ";
+    // cout << "B: " << rowB[i] << endl;
+    cout << rowA[i];
+    if (i < n - 1) cout << " ";
+  }
+
+  cout << endl;
+
+  for (int i = 0; i < n; i++)
+  {
+    // cout << "A: " << rowA[i] << " ";
+    // cout << "B: " << rowB[i] << endl;
+    cout << rowB[i];
+    if (i < n - 1) cout << " ";
+  }
+
+  cout << endl;
 
   return 0;
 }
