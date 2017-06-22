febdcd2100738b011efb98e7b5ec1189977a6712
Mochamad Iqbal Dwi Cahyo <razeluxe20@gmail.com>
2017-06-22 20:39:13 +0700

    [Update] #14 Pendalaman String - B Pembuangan String - Trouble erasing the string, i & j position will exceed the previous   string xD

--- 1114_string/02_string_deletion/practice.cpp
+++ 1114_string/02_string_deletion/practice.cpp
@@ -2,19 +2,21 @@
 #include <string>
 using namespace std;
 
-// string deletion(string s1, string s2)
-int deletion(string s1, string s2)
+string deletion(string s1, string s2)
+// int deletion(string s1, string s2)
 {
+  int i, j = 0;
   int len1 = s1.length();
   int len2 = s2.length();
   bool match;
+  string temp = s1;
   string res = "";
   int count = 0;
 
-  for (int i = 0; i < (len1 - len2 + 1); i++)
+  for (i = 0; i < (len1 - len2 + 1); i++)
   {
     match = true;
-    for (int j = 0; j < len2; j++)
+    for (j = 0; j < len2; j++)
     {
       if (s1[i+j] != s2[j])
       {
@@ -25,18 +27,22 @@ int deletion(string s1, string s2)
 
     if (match)
     {
+      temp = s1;
       count++;
+      cout << "i: " << i << endl;
+      cout << "delete: " << temp.erase(i, j) << endl;
     }
-
-    res += s1[i] + '-';
   }
 
-  // return res;
-  return count;
+  cout << "res: " << res <<endl;
+  return res;
+  // return count;
 }
 
 int main()
 {
+  cout << "s1: serbaser" << endl;
+  cout << "s2: ser" << endl;
   cout << deletion("serbaser", "ser") << endl;
   return 0;
 }
