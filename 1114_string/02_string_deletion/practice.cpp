def5be38bf0e7b470cc3c06c1425f9ec0c14458f
Mochamad Iqbal Dwi Cahyo <razeluxe20@gmail.com>
2017-06-23 01:04:03 +0700

    [Update] #14 Pendalaman String - B Pembuangan String - correctly shows string deletion

--- 1114_string/02_string_deletion/practice.cpp
+++ 1114_string/02_string_deletion/practice.cpp
@@ -8,41 +8,46 @@ string deletion(string s1, string s2)
   int i, j = 0;
   int len1 = s1.length();
   int len2 = s2.length();
+  bool correct[len1];
   bool match;
-  string temp = s1;
   string res = "";
   int count = 0;
 
+  // init correct
+  for (i = 0; i < len1; i++)
+  {
+    correct[i] = true;
+  }
+
   for (i = 0; i < (len1 - len2 + 1); i++)
   {
-    match = true;
+    // match = true;
     for (j = 0; j < len2; j++)
     {
       if (s1[i+j] != s2[j])
       {
-        match = false;
+        // match = false;
         break;
       }
+      correct[i+j] = false;
     }
 
-    if (match)
+    // if (match)
+    if (correct[i])
     {
-      temp = s1;
-      count++;
-      cout << "i: " << i << endl;
-      cout << "delete: " << temp.erase(i, j) << endl;
+      res = res + s1[i];
     }
   }
 
-  cout << "res: " << res <<endl;
+  // cout << "res: " << res <<endl;
   return res;
   // return count;
 }
 
 int main()
 {
-  cout << "s1: serbaser" << endl;
-  cout << "s2: ser" << endl;
+  // cout << "s1: serbaser" << endl;
+  // cout << "s2: ser" << endl;
   cout << deletion("serbaser", "ser") << endl;
   return 0;
 }
