9990e323b9af2e0952113adf764bd0dd9aa467cf
Mochamad Iqbal Dwi Cahyo <razeluxe20@gmail.com>
2017-06-25 09:25:12 +0700

    [Update] #14 Pendalaman String - D Operasi String - String deletion from S2

--- 1114_string/04_string_operation/practice.cpp
+++ 1114_string/04_string_operation/practice.cpp
@@ -3,12 +3,33 @@
 using namespace std;
 
 const int limit = 101;
+char s1[limit], s2[limit], s3[limit], s4[limit];
 
 string modify(string s1, string s2, string s3, string s4)
 {
-  string res = s1 + s2 + s3 + s4;
+  int i, j, k;
+  bool match;
+  
+  for (i = 0; i < s1.length() - s2.length() + 1; i++)
+  {
+    match = true;
+    for (j = 0; j < s2.length(); j++)
+    {
+      if (s1[i+j] != s2[j])
+      {
+        match = false;
+        break;
+      }
+    }
 
-  return res;
+    if (match)
+    {
+      string del = s1.erase(i, j);
+      i -= j;
+    }
+  }
+
+  return s1;
 }
 
 int main()
