91caf3e5d2afd9770e095a213df7844f499f7894
Mochamad Iqbal Dwi Cahyo <razeluxe20@gmail.com>
2017-07-04 20:52:06 +0700

    [Update] #14 Pendalaman String - D Operasi String - 100 AC mwuahaha - I separate deletion and insertion, this way safer

--- 1114_string/04_string_operation/practice.cpp
+++ 1114_string/04_string_operation/practice.cpp
@@ -4,10 +4,11 @@ using namespace std;
 
 const int limit = 101;
 char s1[limit], s2[limit], s3[limit], s4[limit];
+string result = "";
 
-string modify(string s1, string s2, string s3, string s4)
+string strDelete(string s1, string s2)
 {
-  int i, j, k;
+  int i, j;
   bool match;
   
   // for (i = 0; i < s1.length() - s2.length() + 1; i++)
@@ -28,11 +29,41 @@ string modify(string s1, string s2, string s3, string s4)
       // cout << "s1: " << s1 << endl;
       // cout << "i: " << i << endl;
       string del = s1.erase(i, j);
-      i -= j;
-      if (i < 0) i *= -1;
-      s1.insert(i, s3);
+      break;
+      // i -= j;
+      // if (i < 0) i *= -1;
       // cout << "i: " << i << endl;
       // cout << "s1: " << s1 << endl;
+    }
+  }
+
+  return s1;
+}
+
+string strInsert(string s1, string s3, string s4)
+{
+  int i, j;
+  bool match;
+
+  for (i = 0; i < s1.length(); i++)
+  {
+    match = true;
+    for (j = 0; j < s3.length(); j++)
+    {
+      if (s1[i+j] != s3[j])
+      {
+        match = false;
+        break;
+      }
+    }
+
+    if (match)
+    {
+      s1.insert(i+j, s4);
+      i = i + j + (s4.length() - 1);
+      // cout << "s4.length(): " << s4.length() << endl;
+      // cout << "s4[0]: " << s4[0] << endl;
+      // cout << "i: " << i << endl;
     }
   }
 
@@ -43,10 +74,12 @@ int main()
 {
   cin.getline(s1, limit);
   cin.getline(s2, limit);
+  result = strDelete(s1, s2);
   cin.getline(s3, limit);
   cin.getline(s4, limit);
+  result = strInsert(result, s3, s4);
 
-  cout << modify(s1, s2, s3, s4) << endl;
+  cout << result << endl;
   
   return 0;
 }
