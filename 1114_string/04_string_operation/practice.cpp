32342dd01c328cb91c464ebd967809790d52f15d
Mochamad Iqbal Dwi Cahyo <razeluxe20@gmail.com>
2017-07-04 18:55:39 +0700

    [Add] String insert function

--- 1114_string/04_string_operation/practice.cpp
+++ 1114_string/04_string_operation/practice.cpp
@@ -10,7 +10,8 @@ string modify(string s1, string s2, string s3, string s4)
   int i, j, k;
   bool match;
   
-  for (i = 0; i < s1.length() - s2.length() + 1; i++)
+  // for (i = 0; i < s1.length() - s2.length() + 1; i++)
+  for (i = 0; i < s1.length(); i++)
   {
     match = true;
     for (j = 0; j < s2.length(); j++)
@@ -24,8 +25,14 @@ string modify(string s1, string s2, string s3, string s4)
 
     if (match)
     {
+      // cout << "s1: " << s1 << endl;
+      // cout << "i: " << i << endl;
       string del = s1.erase(i, j);
       i -= j;
+      if (i < 0) i *= -1;
+      s1.insert(i, s3);
+      // cout << "i: " << i << endl;
+      // cout << "s1: " << s1 << endl;
     }
   }
 
