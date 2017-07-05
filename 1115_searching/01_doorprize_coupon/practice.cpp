85c2a07386f26b63a9303c9ac434257ad538263d
Mochamad Iqbal Dwi Cahyo <razeluxe20@gmail.com>
2017-07-05 17:13:02 +0700

    [Add] #15 Pencarian - A Kupon Berhadiah - Show deviance for each coupon

--- 1115_searching/01_doorprize_coupon/practice.cpp
+++ 1115_searching/01_doorprize_coupon/practice.cpp
@@ -9,16 +9,19 @@ int x = 0; // coupon x
 int main()
 {
   cin >> n;
+  cin >> x;
   long coupon[n];
+  int min[n];
 
   for (int i = 0; i < n; i++)
   {
     cin >> coupon[i];
+    min[i] = ((coupon[i] - x) < 0) ? (coupon[i] - x) * -1 : (coupon[i] - x); // absolute value, deviance
   }
-  
+
   for (int i = 0; i < n; i++)
   {
-    cout << coupon[i];
+    cout << min[i];
     if (i < n-1) cout << " ";
     else cout << endl;
   }
