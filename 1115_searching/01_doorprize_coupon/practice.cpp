6f891447c6fc9f2deef7fdfaeb52fb795257cdc8
Mochamad Iqbal Dwi Cahyo <razeluxe20@gmail.com>
2017-07-06 17:37:52 +0700

    [Add] #15 Pencarian - A Kupon Berhadiah - 100 AC, using other reference. - Having 3 conditional param to check for x == coupon[i]; x > coupon[i]; first attempt on this row

--- 1115_searching/01_doorprize_coupon/practice.cpp
+++ 1115_searching/01_doorprize_coupon/practice.cpp
@@ -1,60 +1,37 @@
-#include <iostream>
-#include <string>
-#include <cmath>
+#include<iostream>
+#include<math.h>
 using namespace std;
 
-const int limit = 101;
-int n = 0; // how much coupon to compare
-int x = 0; // coupon x
+int main(){
+	long P, T, X, W = 0, WD = 0, i;
 
-int main()
-{
-  cin >> n;
-  cin >> x;
-  long coupon[n];
-  long value = 0;
-  bool couponBool[n];
-  int min[n];
-  int tempMin;
+	cin >> T >> X;
 
-  for (int i = 0; i < n; i++)
-  {
-    couponBool[i] = false;
-    cin >> coupon[i];
-    min[i] = (coupon[i] < x) ? (x - coupon[i]) : (coupon[i] - x);
-    // min[i] = ((coupon[i] - x) < 0) ? (coupon[i] - x) * -1 : (coupon[i] - x); // absolute value
-    // cout << "min["<<i<<"]: " << min[i] << endl;
-  }
+	long N[T], D[T];
 
-  // find minimum value
-  tempMin = min[0];
-  for (int i = 0; i < n; i++)
-  {
-    if (min[i] <= tempMin)
-    {
-      couponBool[i] = true;
-      tempMin = min[i];
-      cout << "tempMin: " << tempMin << endl;
-      // cout << "i: " << i << endl;
-    }
-  }
+	for(i = 0; i < T; i++){
+		cin >> N[i];
+		D[i] = abs(X - N[i]);
+	}
 
-  for (int i = 0; i < n; i++)
-  {
-    // cout << "couponBool["<<i<<"]: " << couponBool[i] << endl;
-    // cout << "coupon["<<i<<"]: " << coupon[i] << endl;
-    // cout << "abs(coupon["<<i<<"] - x): " << abs(coupon[i] - x) << endl;
-    if (couponBool[i] && (abs(coupon[i] - x) == tempMin))
-    {
-      if (coupon[i] == x)
-      {
-        continue;
-      }
-      // break;
-    } 
-  }
+	for(i = 0; i < T; i++){
+		if(i == 0){
+			WD = D[i];
+			P = i;
+		}else{
+			if(WD > D[i]){
+				WD = D[i];
+				P = i;
+			}else if(WD == D[i]){
+				if(N[P] > N[i]){
+					P = i;
+				}
+			}
+		}
+    W = N[P];
+	}
+  
+	cout << W << endl;
 
-  cout << endl;
-
-  return 0;
+	return 0;
 }
